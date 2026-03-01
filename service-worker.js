714d8cb9fc74146c8da31584b1f960ff2dc4a152
Jerbeth Cabusas <jerbethsantiago28@gmail.com>
2026-03-01 18:30:48 +0800

    agin

--- service-worker.js
+++ service-worker.js
@@ -1,15 +1,37 @@
-self.addEventListener("install", function(e){
-    e.waitUntil(
-        caches.open("app-cache").then(function(cache){
-            return cache.addAll(["home.html","home.css","notes.html","notes.css","quiz.html","quiz.css","login.html","login.css","signup.html","profile.html","profile.css"]);
-        })
-    );
+const CACHE_NAME = "app-dev-cache-v1";
+
+const urlsToCache = [
+  "/",
+  "/home.html",
+  "/login.html",
+  "/signup.html",
+  "/notes.html",
+  "/profile.html",
+  "/quiz.html",
+  "/home.css",
+  "/login.css",
+  "/notes.css",
+  "/profile.css",
+  "/quiz.css",
+  "/manifest.json",
+  "/icon-192.png",
+  "/icon-512.png"
+];
+
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        return cache.addAll(urlsToCache);
+      })
+  );
 });
 
-self.addEventListener("fetch", function(e){
-    e.respondWith(
-        caches.match(e.request).then(function(response){
-            return response || fetch(e.request);
-        })
-    );
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        return response || fetch(event.request);
+      })
+  );
 });
